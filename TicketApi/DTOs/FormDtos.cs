4dd8580cf753bd802327dcdfe410743795855895
vollteer <149695869+vollteer@users.noreply.github.com>
2025-07-08 11:39:03 +0200

    Update FormDtos.cs

--- TicketApi/DTOs/FormDtos.cs
+++ TicketApi/DTOs/FormDtos.cs
@@ -488,7 +488,7 @@ namespace YourProject.Models.Forms
 
     public class ImportFormConfigurationResult
     {
-        public bool Success { get; set; }
+        public bool IsSuccess { get; set; }
         public List<FormConfigurationDto> ImportedForms { get; set; } = new();
         public List<string> Errors { get; set; } = new();
         public List<string> Warnings { get; set; } = new();
